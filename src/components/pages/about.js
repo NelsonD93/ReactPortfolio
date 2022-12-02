2fb81a13713e69ee5122327abdfb3305098bec5c
Nelson Drozd <nelsondrozd@gmail.com>
2022-12-02 18:18:22 -0500

    Added about section, started working on contact

---
+++ src/components/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../styles/Mystyles.css';
+import image from '../../img/106918045.jpg';
+
+
+function About () {
+    return (
+        <div>
+      <h1 className='pageTitle'>About Me</h1>
+      <div className='myContent'>
+        <img src={image} alt="Logo" width ="10%"/>
+        </div>
+      <p>
+        I am a SaaS professional who loves all things Tech. From building custom PC's for the last 10 years to now web development.  I am attending a Full Stack Developer bootcamp and set to graduate in mid-December 2022. I am beginning my search for junior web developer roles and would love to hear from you.
+      </p>
+      
+    </div>
+    )
+}
+
+export default About;
---
+++ src/components/pages/contact.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import '../../styles/Mystyles.css';
+import { validateEmail } from '../utils/helpers';
+
